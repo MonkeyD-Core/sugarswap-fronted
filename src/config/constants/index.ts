f5e183d63e9950239e9ad732c40a434eef025848
Saka <62377315+MonkeyD-Core@users.noreply.github.com>
2021-08-06 18:42:05 +0800

    Update index.ts

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@pancakeswap/sdk'
 import { BUSD, DAI, USDT, BTCB, CAKE, WBNB, UST, ETH, USDC } from './tokens'
 
-export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+export const ROUTER_ADDRESS = '0xA9635AAbd0CC720919901dCE66F0895C61b6f5Bc'
 
 // a list of tokens by chain
 type ChainTokenList = {
