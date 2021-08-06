5116bf48caa6192a3d0f791ee56a1194add151bf
MonkeyD-Core <imskaa@gdrivemail.com>
2021-08-06 08:32:18 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Menu as UikitMenu } from '@pancakeswap/uikit'
+import { languageList } from 'config/localization/languages'
+import { useTranslation } from 'contexts/Localization'
+import useTheme from 'hooks/useTheme'
+import { usePriceCakeBusd } from 'state/farms/hooks'
+import { useProfile } from 'state/profile/hooks'
+import config from './config'
+import UserMenu from './UserMenu'
+
+const Menu = (props) => {
+  const { isDark, toggleTheme } = useTheme()
+  const cakePriceUsd = usePriceCakeBusd()
+  const { profile } = useProfile()
+  const { currentLanguage, setLanguage, t } = useTranslation()
+
+  return (
+    <UikitMenu
+      userMenu={<UserMenu />}
+      isDark={isDark}
+      toggleTheme={toggleTheme}
+      currentLang={currentLanguage.code}
+      langs={languageList}
+      setLang={setLanguage}
+      cakePriceUsd={cakePriceUsd.toNumber()}
+      links={config(t)}
+      profile={{
+        username: profile?.username,
+        image: profile?.nft ? `/images/nfts/${profile.nft?.images.sm}` : undefined,
+        profileLink: '/profile',
+        noProfileLink: '/profile',
+        showPip: !profile?.username,
+      }}
+      {...props}
+    />
+  )
+}
+
+export default Menu
